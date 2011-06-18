d29d77be641219a5969a548595a59d96c1f58d28
jqlblue <gaoyuan.blue@gmail.com>
2011-06-18 20:03:34 +0800

    fix autoload

--- code/demo/init.php
+++ code/demo/init.php
@@ -1,8 +1,7 @@
 <?php
 define("ROOT_DIR", dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
 define('DS', DIRECTORY_SEPARATOR);
-//echo ROOT_DIR;exit;
-set_include_path(get_include_path() . PATH_SEPARATOR . ROOT_DIR . 'library/');
+//set_include_path(get_include_path() . PATH_SEPARATOR . ROOT_DIR . 'library/');
 /**
  * 自动加载
  * @param className $class
@@ -10,13 +9,17 @@ set_include_path(get_include_path() . PATH_SEPARATOR . ROOT_DIR . 'library/');
 function base_autoload($class)
 {
     $tmp = explode('_', $class);
-    if(count($tmp) > 1) {
-        $file = ROOT_DIR. 'application' . DS  . strtolower(implode(DS, $tmp)).'.php';
-        if(file_exists($file)) {
-            include_once($file);
-        }
+    if (count($tmp) > 1){
+        $file = strtolower(implode(DS, $tmp)) . '.php';
+    }else {
+        $file = $class . '.php';
+    }
+
+    $file = ROOT_DIR . 'library/' . $file;
+    if (file_exists($file))
+    {
+        include_once($file);
     }
 }
-//spl_autoload_register('base_autoload');
-//Zend_Loader_Autoloader::getInstance()->setFallbackAutoloader(true)->pushAutoloader('baccarat_autoload');
+spl_autoload_register('base_autoload');
 
